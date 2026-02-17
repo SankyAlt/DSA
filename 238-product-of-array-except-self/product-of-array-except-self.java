8ca45f3fee4a7dfd46f3def5f7f2fbd13fe29e66
Sanskar Rajesh Patil <sanskarpatil122006@gmail.com>
2026-02-17 19:24:16 +0530

    Time: 3 ms (26.86%) | Memory: 72.1 MB (42.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,20 +2,21 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         
         int[] ans = new int[nums.length];
-        int[] right = new int[nums.length];
+        
         ans[0] = 1;
         for(int i=1; i<nums.length; i++){
             ans[i] = ans[i-1]*nums[i-1];
         }//left mult of each elements is stored in ans
         
-        right[nums.length-1] = 1;
-        for(int j=nums.length-1-1; j>=0; j--){
-            right[j] = right[j+1]*nums[j+1];
+        int right = 1;
+
+        //without storing right mults
+        for(int j=nums.length-1; j>=0; j--){
+            ans[j] = ans[j]*right;
+            right = right*nums[j];
             
-        }//right mult of each element is stored in right
-        for(int k=0; k<nums.length; k++){
-            ans[k] = ans[k]*right[k];
         }
+        
         return ans;
 
     }
