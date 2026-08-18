1f57567e1acda36df33283e501d8d69d5b0ba2ea
Sanskar Rajesh Patil <sanskarpatil122006@gmail.com>
2026-08-18 11:27:20 +0530

    Time: 34 ms (60.11%) | Memory: 58.7 MB (93.72%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,58 +1,40 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate first numbers
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        
+        for(int i = 0;i < n-2; i++){
+            int left = i + 1;
+            int right = n - 1;
+            int sum = -nums[i];
+            if (i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
-
-            // Since array is sorted, no possible triplet after this
-            if (nums[i] > 0) {
-                break;
-            }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(
-                        nums[i],
-                        nums[left],
-                        nums[right]
-                    ));
-
-                    // Skip duplicate second numbers
-                    while (left < right && nums[left] == nums[left + 1]) {
+            while (left < right){
+                if (sum == (nums[left] + nums[right])){
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < n && nums[left] == nums[left-1]){
                         left++;
                     }
-
-                    // Skip duplicate third numbers
-                    while (left < right && nums[right] == nums[right - 1]) {
+                    while(right >= 0 && nums[right] == nums[right+1]){
                         right--;
                     }
 
+                }
+                else if (sum > (nums[left] + nums[right])){
                     left++;
-                    right--;
-
-                } else if (sum < 0) {
-                    left++;
-
-                } else {
+                }
+                else{
                     right--;
                 }
-            }
-        }
 
-        return result;
+            }
+            
+        }
+        return res;
+        
     }
 }
